20760ace49c59bb864c6502870b3ddf0703b1114
Lenny Peters <mr.lennypeters@gmail.com>
2020-06-07 15:36:43 -0700

    refactor: 💡 history.js
    adding a history object to use for changing navigation programmatically

--- src/streams/StreamList.jsx
+++ src/streams/StreamList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchStreams } from "../actions";
 
@@ -7,11 +8,27 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button type="button" className="ui button primary">
+            Edit
+          </button>
+          <button type="button" className="ui button negative">
+            Delete
+          </button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     const { streams } = this.props;
     return streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -22,18 +39,36 @@ class StreamList extends Component {
     });
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <div className="container">
         <h2>StreamList</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(
