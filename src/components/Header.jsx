9443e5650296603c76c119eefd2e4ae2f439c17e
Lenny Peters <mr.lennypeters@gmail.com>
2020-06-12 21:29:46 -0700

    refactor: 💡 Header.jsx
    abstracted hardcoded values into variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
+  const translations = {
+    appName: "Streamer",
+    allStreams: "All Streams"
+  };
+  const { appName, allStreams } = translations;
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        Streamer
+        {appName}
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
-          All Streams
+          {allStreams}
         </Link>
         <GoogleAuth />
       </div>
